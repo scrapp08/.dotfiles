70a58fb8f46be18314c8e9f2d537d6a733c67930
Scrap <105889363+scrapp08@users.noreply.github.com>
2025-01-31 19:39:21 +0000

    Updated install.sh and removed file

--- install.sh
+++ install.sh
@@ -75,44 +75,14 @@ download_dotfiles() {
 
 
 link_dotfiles() {
-  # symlink files to the HOME directory.
-  if [[ -f "${DIR}/opt/files" ]]; then
-    _process "→ Symlinking dotfiles in /configs"
-
-    # Set variable for list of files
-    files="${DIR}/opt/files"
-
-    # Store IFS separator within a temp variable
-    OIFS=$IFS
-    # Set the separator to a carriage return & a new line break
-    # read in passed-in file and store as an array
-    IFS=$'\r\n'
-    links=($(cat "${files}"))
-
-    # Loop through array of files
-    for index in ${!links[*]}
-    do
-      for link in ${links[$index]}
-      do
-        _process "→ Linking ${links[$index]}"
-        # set IFS back to space to split string on
-        IFS=$' '
-        # create an array of line items
-        file=(${links[$index]})
-        # Create symbolic link
-        ln -fs "${DIR}/${file[0]}" "${HOME}/${file[1]}"
-      done
-      # set separater back to carriage return & new line break
-      IFS=$'\r\n'
-    done
-
-    # Reset IFS back
-    IFS=$OIFS
-
-    source "${HOME}/.bash_profile"
-
-    [[ $? ]] && _success "All files have been copied"
-  fi
+	_process "→ Sym-linking dotfiles"
+	ln -s ~/.dotfiles/configs/gitconfig ~/.gitconfig
+	ln -s ~/.dotfiles/configs/nvim/ ~/.config/nvim
+	ln -s ~/.dotfiles/configs/p10k.zsh ~/.p10k.zsh
+	ln -s ~/.dotfiles/configs/vimrc ~/.vimrc
+	ln -s ~/.dotfiles/configs/yt-dlp/ ~/.config/yt-dlp
+	ln -s ~/.dotfiles/configs/zshrc ~/.zshrc
+	_success "Linked Dotfiles"
 }
 
 

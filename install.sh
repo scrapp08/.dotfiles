9f67404778e8a94697715e8bbe7ee49daa883ebe
Scrap <105889363+scrapp08@users.noreply.github.com>
2025-01-31 18:52:14 +0000

    Updated install.sh

--- install.sh
+++ install.sh
@@ -17,16 +17,40 @@ _success() {
 }
 
 
-setup() {
+install_xcode-select() {
+	_process"→ Installing Xcode command-line tools"   
 	xcode-select --install
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	xcode-select -v
+	[[ $? ]] \
+	&& _success "Installed Xcode command-line tools"
+}
+
+
+install_homebrew() {
+	_process"→ Installing Homebrew"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ed/.zprofile
 	eval "$(/opt/homebrew/bin/brew shellenv)"
+
+	_process "→ Running brew doctor"
+	brew doctor
+	[[ $? ]] \
+	&& _success "Installed Homebrew"
+}
+
+
+install_misc() {
+	_process"→ Installing brews"
 	brew bundle --file ~/.dotfiles/etc/Brewfile
+	_success "Installed brews"
+	_process"→ Installing OhMyZsh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	_success "Installed OhMyZsh"
+	_process"→ Installing Zsh plugins"
 	git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	_success "Installed Zsh plugins"
 }
 
 
@@ -93,7 +117,9 @@ link_dotfiles() {
 
 
 install() {
-  setup
+  install_xcode-select
+  install_homebrew  
+  install_misc
   download_dotfiles
   link_dotfiles
 }
